906f28deb3289c0e13b08c9e8ee83953d56397a4
Chad Cumba <chad.cumba@mail.utexas.edu>
2011-01-28 15:03:59 -0600

    Added in filter to remove unassigned games from staff view

--- game/admin.py
+++ game/admin.py
@@ -5,6 +5,16 @@ from game.models import Game, Outcome
 from django.contrib import admin
 
 class GameAdmin(admin.ModelAdmin):
-    list_display=('__unicode__','game_complete','score')
+    list_display=('__unicode__','game_complete','score','cash_amount',
+                  'condition')
+    list_filter=('condition',)
+    search_fields = ['user__username']
+    
+    def queryset(self, request):
+        qs = super(GameAdmin,self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user__game__isnull=False)
+    
 admin.site.register(Game, GameAdmin)
 admin.site.register(Outcome)
